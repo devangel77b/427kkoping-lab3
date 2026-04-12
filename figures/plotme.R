4ff5a6b586d6b1fbaa7e29277935e873b88e4fbf
Dennis Evangelista <devangel77b@gmail.com>
2026-04-12 10:11:06 -0400

    inconsistencies in KE and GPE; and in F vs displacement

---
+++ figures/plotme.R
@@ -0,0 +1,22 @@
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+
+raw <- read.csv('kinematics.csv',header=TRUE)
+data <- tibble(raw)
+m=0.0060
+g=9.81
+
+data <- mutate(data,
+     KE=1/2*m*v0^2,
+     GPE=m*g*h)
+     
+# make a bar graph of energy
+long <- pivot_longer(data,
+     cols = c(KE,GPE),
+     names_to = "type",
+     values_to = "energy")
+Efig <- ggplot(long,aes(x=type,y=energy,fill=type))+
+     stat_summary(geom="bar",fun=mean)+
+     stat_summary(geom="errorbar",
+	fun.data=mean_sdl,fun.args=list(mult=1),width=0.2)
